b18a55921055d36738c8a9d3922ed6b2994db277
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-10 14:19:26 +0530

    Add feature modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChatModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatboxComponent } from './chatbox/chatbox.component';
+
+@NgModule({
+  declarations: [ChatboxComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChatModule { }
