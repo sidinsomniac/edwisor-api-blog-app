ab5b5bc9cc1cf1bfc43bf7f1c86df15ec9648db6
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-12 23:04:17 +0530

    Initialize chat functionality

--- src/app/chat/chatbox/chatbox.component.ts
+++ src/app/chat/chatbox/chatbox.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SocketService } from 'src/app/socket.service';
 import { AppService } from 'src/app/app.service';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-chatbox',
@@ -20,10 +21,59 @@ export class ChatboxComponent implements OnInit {
   constructor(
     private socketService: SocketService,
     private appService: AppService,
+    private cookie: CookieService,
     private router: Router
-  ) { }
+  ) {
+    this.receiverId = cookie.get('receiverId');
+    this.receiverName = cookie.get('receiverName');
+   }
 
   ngOnInit() {
+    this.authToken = this.cookie.get('authtoken');
+    this.userInfo = this.appService.getUserInfoLocalStorage();
+    this.checkStatus();
+    this.verifyUserConfirmation();
+    this.getOnlineUserList();
+  }
+
+  public checkStatus() {
+    if (
+        this.cookie.get('authtoken') === undefined ||
+        this.cookie.get('authtoken') === '' || 
+        this.cookie.get('authtoken') === null
+        ) {
+      this.router.navigate(['/']);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public verifyUserConfirmation() {
+    this.socketService.verifyUser().subscribe(
+      data => {
+        this.disconnectedSocket = false;
+        this.socketService.setUser(this.authToken);
+        this.getOnlineUserList();
+      }
+    )
+  }
+
+  public getOnlineUserList() {
+    this.socketService.onlineUserList().subscribe(
+      userList => {
+        this.userList = [];
+        for (let list in userList) {
+          let temp = {
+            'userId': list,
+            'name': userList[list],
+            'unread': 0,
+            'chatting': false
+          }
+          this.userList.push(temp);
+        }
+      }
+    )
   }
 
 }
